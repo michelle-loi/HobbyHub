4c01e6f58ef2c6d1c548957a7066b2fc4c1b37ff
MichelleL <168042638+michelle-loi@users.noreply.github.com>
2024-03-13 03:08:55 -0600

    fix(under development page): removed periods in text

--- client/src/pages/underdevelopment/UnderDevelopment.jsx
+++ client/src/pages/underdevelopment/UnderDevelopment.jsx
@@ -17,8 +17,8 @@ const UnderDevelopment = () =>{
                 <Col className="d-flex flex-column justify-content-center align-items-center notfound-col">
                     <div className="d-flex flex-column justify-content-center align-items-center">
                         <h1 className="text-white fw-bolder">This Page Is Under Development</h1>
-                        <h1 className="text-white fw-bolder">Some Journey Too Far Into The Great Unknown.</h1>
-                        <h1 className="text-white fw-bolder">There Is Nothing Here For You.</h1>
+                        <h1 className="text-white fw-bolder">Some Journey Too Far Into The Great Unknown</h1>
+                        <h1 className="text-white fw-bolder">There Is Nothing Here For You</h1>
                         <Link onClick={goBack} to="#"><h2 className="text-white fw-bolder">It Is Time To Return To Your World!</h2></Link>
                     </div>
                 </Col>
