ec4006b138e81f4772273c6cd7b0c3de770b2064
MichelleL <168042638+michelle-loi@users.noreply.github.com>
2024-03-13 01:56:37 -0600

    feat(signup schema): schema for checking signup form input fields

---
+++ client/src/pages/authentication/schemas/SignupSchema.jsx
@@ -0,0 +1,37 @@
+import * as yup from "yup";
+
+const oneUpper = /(?=.*[A-Z])/;
+const oneLower = /(?=.*[a-z])/;
+const oneDigit = /(?=.*\d)/;
+const oneSymbol = /(?=.*[!@#$%^&*()_+{}\[\]:;<>,.?~\\|\-=/"'])/;
+
+export const SignupSchema = yup.object().shape({
+    username: yup
+        .string()
+        .min(4, "Username must be 4 or more characters")
+        .required("Username is required"),
+
+    email: yup
+        .string()
+        .email("Please enter a valid email")
+        .required("Email is required"),
+
+    password: yup
+        .string()
+        .min(4, "Password must be 4 or more characters")
+        .matches(oneUpper, {message: "Password should contain at least one uppercase letter"})
+        .matches(oneLower, {message: "Password should contain at least one lowercase letter"})
+        .matches(oneDigit, {message: "Password should contain at least one digit"})
+        .matches(oneSymbol, {message: "Password should contain at least one special character"})
+        .required("Password is required"),
+
+    confirmPassword: yup
+        .string()
+        .required('Please confirm your password')
+        .oneOf([yup.ref('password'), null], 'Passwords must match'),
+
+    birthdate: yup
+        .date()
+        .max(new Date(new Date().getFullYear() - 12, new Date().getMonth(), new Date().getDate()), "Website's age restriction is 12")
+        .required('Birthdate is required'),
+})
