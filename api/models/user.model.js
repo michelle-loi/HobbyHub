bdcd32f912e4b4cacc709279476e8a5c6dbc6342
Ryan <ryan.loi@ucalgary.ca>
2024-03-12 23:31:04 -0600

    feat(schema): add user.model.js schema
    Add schema definition for the user entity.
    
    BREAKING CHANGE: None

--- api/models/user.model.js
+++ api/models/user.model.js
@@ -1,15 +1,36 @@
-// This is to create schemas for the mongodb database
-// 13:39 on video
-
 import mongoose from 'mongoose';
 const { Schema } = mongoose;
 
 // User schema model
 const userSchema = new Schema({
-    // username field for the userschema
+    // username field for the user schema
     username:{
         type: String,
-        required:true, // making usernames required
-        unique:true, // making the username unique
+        required:true,
+        unique:true,
     },
+    email:{
+        type:String,
+        required:true,
+        unique:true
+    },
+    birthday:{
+        type:Date,
+        required:false
+    },
+    password:{
+        type:String,
+        required:true
+    },
+    phone:{
+        type:Number,
+        required:false
+    },
+    marketplaceRating: {
+        type: Number,
+        default:0
+    }
 });
+
+
+export default mongoose.model("User", userSchema)
