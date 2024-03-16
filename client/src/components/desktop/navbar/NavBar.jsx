9c85bc5e7f7d4097fde82a4573625c417c98a2e7
MichelleL <168042638+michelle-loi@users.noreply.github.com>
2024-03-15 18:03:25 -0600

    feat(navbar): implemented a navbar with search and login button. Also contains a hamburger button which the sidemenu collapses into on smaller viewports. Searchbar also collapses into a button at smaller screen sizes.

--- client/src/components/desktop/navbar/NavBar.jsx
+++ client/src/components/desktop/navbar/NavBar.jsx
@@ -1,12 +1,99 @@
-import React from "react";
+import React, {useEffect, useRef, useState} from "react";
 import "./NavBar.scss"
+import {Button, Navbar, Nav, Offcanvas, Container, Form, Row, Col, InputGroup} from 'react-bootstrap';
+import LeftMenu from "../leftmenu/LeftMenu.jsx";
+import {useMediaQuery} from "react-responsive";
+import Logo from "../../../assets/authentication/mobile/logo.svg"
+import Search from "../../../assets/navbar/search.svg"
 
-const NavBar = () =>{
+const NavBar = () => {
+
+    const [showSidebar, setShowSidebar] = useState(false);
+
+    const handleClose = () => setShowSidebar(false);
+    const handleShow = () => setShowSidebar(!showSidebar);
+
+    const matches = useMediaQuery({query: `(min-width: 1200px)`});
+
+    useEffect(() => {
+        if (matches) {
+            setShowSidebar(false);
+        }
+    }, [matches]);
+
+    const [searchVisible, setSearchVisible] = useState(false);
+    const [prevSearchVisible, setPrevSearchVisible] = useState(false);
+    const searchRef = useRef(null);
+    const isBelow991px = useMediaQuery({
+        query: '(max-width: 991px)'
+    });
+
+    useEffect(() => {
+        const handleClickOutside = (event) => {
+            if (searchRef.current && !searchRef.current.contains(event.target)) {
+                setSearchVisible(false);
+            }
+        };
+
+        document.addEventListener('click', handleClickOutside);
+        return () => {
+            document.removeEventListener('click', handleClickOutside);
+        };
+    }, []);
+
+    const handleSearchToggle = () => {
+        setSearchVisible(!searchVisible);
+    };
+
+    useEffect(() => {
+        if (!isBelow991px && prevSearchVisible) {
+            setSearchVisible(false);
+        }
+        setPrevSearchVisible(searchVisible);
+    }, [isBelow991px, prevSearchVisible, searchVisible]);
+
     return (
-        <div>
-            NavBar
-        </div>
-    )
+        <>
+            {/* Navbar */}
+            <Navbar expand="xl" className="bg-white fixed-top desktop-navbar">
+                <Container fluid>
+                    <Row className="w-100">
+                        <Col className="d-flex align-items-center justify-content-between">
+
+                            <div>
+                                <Navbar.Toggle className="navbar-toggle" aria-controls="basic-navbar-nav"
+                                               onClick={handleShow}/>
+
+                                <Navbar.Brand className="me-auto ms-3" href="/">
+                                    <img className="navbar-logo" src={Logo} alt="HobbyHub"/>
+                                </Navbar.Brand>
+                            </div>
+
+                            <div ref={searchRef} className={`d-searchbar ${searchVisible && isBelow991px ? 'search-visible' : ''}`}>
+
+                                <button className="search-icon" onClick={handleSearchToggle}>
+                                    <img src={Search} alt="search" />
+                                </button>
+
+                                <Form.Control type="text" placeholder="Search"></Form.Control>
+                            </div>
+
+                            <Button variant="HHPurple"> Log in</Button>
+                        </Col>
+                    </Row>
+                </Container>
+            </Navbar>
+
+            {/* Sidebar */}
+            <Offcanvas show={showSidebar} onHide={handleClose} scroll={true} backdrop={true} className="home-sidebar">
+                <Offcanvas.Body className="m-0 p-0 test">
+                    <Nav className="flex-column">
+                        <LeftMenu/>
+                    </Nav>
+                </Offcanvas.Body>
+            </Offcanvas>
+        </>
+    );
 }
 
 export default NavBar
