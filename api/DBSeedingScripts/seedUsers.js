79950f4629895a460523e93c0fdbab5544d8a479
Ryan <ryanloi273@gmail.com>
2024-03-17 22:55:21 -0600

    fix: encrypt passwords with bcryptjs in seedUsers.js script
    Fixed seedUsers.js script to properly encrypt passwords using bcryptjs before uploading the database.
    
    BREAKING CHANGE: None

--- api/DBSeedingScripts/seedUsers.js
+++ api/DBSeedingScripts/seedUsers.js
@@ -1,6 +1,8 @@
 import mongoose from 'mongoose';
 import User from '../models/user.model.js';
 import dotenv from "dotenv";
+import bcrypt from 'bcryptjs';
+
 
 // configure dotenv to read from .env files
 dotenv.config();
@@ -9,7 +11,7 @@ const data = [
     {
         username: 'user1',
         email: 'user1@email.com',
-        password: 'user1pw',
+        password: 'User1!',
         birthday: new Date("2000-05-11"),
         phone: 4031021111
     },
@@ -17,7 +19,7 @@ const data = [
     {
         username: 'user2',
         email: 'user2@email.com',
-        password: 'user2pw',
+        password: 'User2!',
         birthday: new Date("1990-02-12"),
         marketplaceRating: 1
     },
@@ -25,9 +27,17 @@ const data = [
     {
         username: 'user3',
         email: 'user3@email.com',
-        password: 'user3pw',
+        password: 'User3!',
         birthday: new Date("1923-01-03"),
-    }
+    },
+
+    {
+        username: 'test',
+        email: 'test@email.com',
+        password: 'Test1!',
+        birthday: new Date("2000-05-11"),
+        phone: 4031021111
+    },
 ];
 
 
@@ -37,7 +47,13 @@ async function seedUserData() {
         await mongoose.connect(process.env.MONGO);
         console.log('Connected to the database');
 
-        await User.insertMany(data);
+        // Hash passwords before inserting user data
+        const hashedData = data.map(user => ({
+            ...user,
+            password: bcrypt.hashSync(user.password, 10) // Hash password with bcrypt
+        }));
+
+        await User.insertMany(hashedData);
         console.log('User Data seeded');
 
         mongoose.connection.close();
