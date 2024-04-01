96418b7e75867101423f4f6145e270b8a6928780
janpetallo <101074771+janpetallo@users.noreply.github.com>
2024-04-01 10:33:07 -0600

    fix(post-location): resolved warning received when rendering choose-posting page

--- client/src/pages/posting-location/ChoosePostingLocation.jsx
+++ client/src/pages/posting-location/ChoosePostingLocation.jsx
@@ -1,23 +1,25 @@
 import React, { useEffect } from 'react';
 import { Button, Container, Row, Col } from 'react-bootstrap';
+import { useMediaQuery } from 'react-responsive';
 import hubsSvg from '../../assets/leftsidemenu/hubs.svg';
 import marketSvg from '../../assets/leftsidemenu/market.svg';
 import { Link, useNavigate } from 'react-router-dom';
 import './ChoosePostingLocation.scss';
 
 const ChoosePostingLocation = () => {
+    const isMobile = useMediaQuery({ query: '(max-width: 575px)' });
     const navigate = useNavigate();
 
     useEffect(() => {
-        const checkResize = () => {
-            if (window.innerWidth < 576) {
+        // const checkResize = () => {
+            if (isMobile) {
                 navigate('/');
             }
-        };
-        window.addEventListener('resize', checkResize);
-        checkResize(); // check immediately on component mount
-        return () => window.removeEventListener('resize', checkResize);
-    }, [navigate]);
+        // };
+        // window.addEventListener('resize', checkResize);
+        // checkResize(); // check immediately on component mount
+        // return () => window.removeEventListener('resize', checkResize);
+    }, [isMobile ,navigate]);
 
     return (
         <Container id="centered-container" className="centered-container">
@@ -30,7 +32,7 @@ const ChoosePostingLocation = () => {
             <Row>
                 <Col>
                     <Link to="/hubpost" id="posting-link">
-                        <Button variant="light" size="lg" block id="square-button">
+                        <Button variant="light" size="lg" block='true' id="square-button">
                             <img src={hubsSvg} alt="Hubs" id="button-svg" />
                             <span>Hubs</span>
                         </Button>
@@ -39,7 +41,7 @@ const ChoosePostingLocation = () => {
                 
                 <Col >
                     <Link to="/marketpost" id="posting-link">
-                        <Button variant="light" size="lg" block id="square-button">
+                        <Button variant="light" size="lg" block='true' id="square-button">
                             <img src={marketSvg} alt="Hubs" id="button-svg" />
                             <span>Market</span>
                         </Button>
