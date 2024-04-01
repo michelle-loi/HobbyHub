f34afcd376a804d82509590f4767b21a7b48cecb
GurleenA <101472021+GurleenA@users.noreply.github.com>
2024-04-01 03:28:24 -0600

    feat(marketplace): implemented the front end for marketplace selection
    Implemented the front end for the marketplace selection feature, enabling users to choose specific categories or sections they want to explore within the marketplace.

---
+++ client/src/pages/marketplace-selection/MarketplaceSelection.jsx
@@ -0,0 +1,40 @@
+import React from "react";
+import { useState } from "react";
+import {Button, Nav, Navbar, NavDropdown, Offcanvas, Row} from "react-bootstrap";
+import {Col, Container, Card, Badge, Image} from "react-bootstrap";
+import {Link, useNavigate} from "react-router-dom";
+import "./MarketplaceSelection.scss"
+
+const MarketplaceSelection = () => {
+    const categories = [
+        { section: 'Outdoor Activities', markets: ['Gardening', 'Fishing', 'Cycling','Hiking & Camping'] },
+        { section: 'Indoor Activites', markets: ['Books', 'Painting'] },
+        { section: 'Sports and Fitness', markets: ['Yoga', 'Team Sports', 'Gym Equipment', 'Swimming'] },
+        { section: 'Games', markets: ['Board Games', 'Card Games', 'Lego'] },
+    ];
+    return(
+        <>
+            <Container className="ps-5">
+                {categories.map((category, i) => (
+                    <Row key={i} className="mt-4 section-font">
+                        {category.section}
+                        <Row>
+                            {/* <Row id={i} className="font-check"> */}
+                            {category.markets.map((market, index) => (
+                                <Col sm={2} xs={4} key={index} className="col-container m-2 p-0">
+                                    <div className="text-center p-1 ">
+                                        {market}
+                                    </div>
+                                </Col>
+                            ))}
+                        </Row>
+                        {/* </Row> */}
+                    </Row>
+                ))}
+            </Container>
+
+        </>
+
+    )
+}
+export default MarketplaceSelection
