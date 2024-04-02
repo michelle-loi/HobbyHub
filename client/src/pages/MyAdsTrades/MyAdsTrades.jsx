37801aad738bedd423fa08883fe66c665a98a9e9
MichelleL <168042638+michelle-loi@users.noreply.github.com>
2024-04-02 00:23:16 -0600

    chore: fixed padding on myhubs, myposts, and myads/trades

--- client/src/pages/MyAdsTrades/MyAdsTrades.jsx
+++ client/src/pages/MyAdsTrades/MyAdsTrades.jsx
@@ -8,7 +8,7 @@ const MyAdsTrades = () => {
     return (
         <>
             <div className="d-flex flex-column align-items-center justify-content-center">
-                <h1 className="pt-2 ps-2 pe-2">{username}'s Ads & Trades</h1>
+                <h1 className="pt-3 ps-2 pe-2">{username}'s Ads & Trades</h1>
                 <h6 className="ps-2 pe-2!">View all your ads and trades here!</h6>
             </div>
             <hr/>
