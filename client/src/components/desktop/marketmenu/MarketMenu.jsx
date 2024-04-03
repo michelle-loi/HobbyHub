c0a866f228e11c6d16d0ef15e1c786390b7f87eb
GurleenA <101472021+GurleenA@users.noreply.github.com>
2024-04-02 22:03:41 -0600

    feat(market-menu): implemented the frontend for the rating stars
    The rating stars allow users to filter by rating.

--- client/src/components/desktop/marketmenu/MarketMenu.jsx
+++ client/src/components/desktop/marketmenu/MarketMenu.jsx
@@ -12,16 +12,16 @@ const MarketMenu = () => {
 
 
         <Container fluid>
-            <div className="d-flex justify-content-center fw-bold">Filter</div>
+            <div className="d-flex justify-content-center fw-bold fs-4">Filter</div>
             <Row >
-                <Accordion className="menu-accordion">
-                    <Accordion.Item eventKey="0" className="mb-5 mt-5 flush">
-                        <Accordion.Header>Type</Accordion.Header>
+                <Accordion className="menu-accordion" flush>
+                    <Accordion.Item eventKey="0" className="mb-5 mt-5">
+                        <Accordion.Header className="menu-header">Type</Accordion.Header>
                         <Accordion.Body>
 
                         </Accordion.Body>
                     </Accordion.Item>
-                    <Accordion.Item eventKey="1" className="mb-5 flush">
+                    <Accordion.Item eventKey="1" className="mb-5">
                         <Accordion.Header>Price</Accordion.Header>
                         <Accordion.Body>
                            <div>
@@ -30,16 +30,50 @@ const MarketMenu = () => {
 
                         </Accordion.Body>
                     </Accordion.Item>
-                    <Accordion.Item eventKey="2" className="mb-5 flush">
+                    <Accordion.Item eventKey="2" className="mb-5">
                         <Accordion.Header>Location</Accordion.Header>
                         <Accordion.Body>
 
                         </Accordion.Body>
                     </Accordion.Item>
-                    <Accordion.Item eventKey="3" className="mb-5 flush">
+                    <Accordion.Item eventKey="3" className="mb-5 border-bottom">
                         <Accordion.Header>Rating</Accordion.Header>
                         <Accordion.Body>
-
+                            <div className="w-100 mb-1">
+                                <Image src={star} alt="rating" className="rating-stars"/>
+                                <Image src={star} alt="rating" className="rating-stars"/>
+                                <Image src={star} alt="rating" className="rating-stars"/>
+                                <Image src={star} alt="rating" className="rating-stars"/>
+                                <Image src={star} alt="rating" className="rating-stars"/>
+                            </div>
+                            <div className="w-100 mb-1">
+                                <Image src={star} alt="rating" className="rating-stars"/>
+                                <Image src={star} alt="rating" className="rating-stars"/>
+                                <Image src={star} alt="rating" className="rating-stars"/>
+                                <Image src={star} alt="rating" className="rating-stars"/>
+                                <Image src={unfilledStar} alt="rating" className="rating-stars"/>
+                            </div>
+                            <div className="w-100 mb-1">
+                                <Image src={star} alt="rating" className="rating-stars"/>
+                                <Image src={star} alt="rating" className="rating-stars"/>
+                                <Image src={star} alt="rating" className="rating-stars"/>
+                                <Image src={unfilledStar} alt="rating" className="rating-stars"/>
+                                <Image src={unfilledStar} alt="rating" className="rating-stars"/>
+                            </div>
+                            <div className="w-100 mb-1">
+                                <Image src={star} alt="rating" className="rating-stars"/>
+                                <Image src={star} alt="rating" className="rating-stars"/>
+                                <Image src={unfilledStar} alt="rating" className="rating-stars"/>
+                                <Image src={unfilledStar} alt="rating" className="rating-stars"/>
+                                <Image src={unfilledStar} alt="rating" className="rating-stars"/>
+                            </div>
+                            <div className="w-100 mb-1">
+                                <Image src={star} alt="rating" className="rating-stars"/>
+                                <Image src={unfilledStar} alt="rating" className="rating-stars"/>
+                                <Image src={unfilledStar} alt="rating" className="rating-stars"/>
+                                <Image src={unfilledStar} alt="rating" className="rating-stars"/>
+                                <Image src={unfilledStar} alt="rating" className="rating-stars"/>
+                            </div>
 
                         </Accordion.Body>
                     </Accordion.Item>
@@ -48,7 +82,7 @@ const MarketMenu = () => {
 
         </Container>
 
-)
+    )
 
 }
 export default MarketMenu
