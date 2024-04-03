38019b24861d854637bb9742655b65084a40f9ff
GurleenA <101472021+GurleenA@users.noreply.github.com>
2024-04-02 21:42:00 -0600

    fix(market-menu): fixed the layout and switched to Accordion

--- client/src/components/desktop/marketmenu/MarketMenu.jsx
+++ client/src/components/desktop/marketmenu/MarketMenu.jsx
@@ -1,5 +1,5 @@
 import React, {useState} from "react";
-import {Button, Col, Container, Row, Dropdown, Image} from "react-bootstrap";
+import {Button, Col, Container, Row, Dropdown, Image, Accordion} from "react-bootstrap";
 import {IoPersonSharp} from "react-icons/io5";
 import "./MarketMenu.scss";
 import star from "../../../assets/marketplace/star.svg";
@@ -14,65 +14,36 @@ const MarketMenu = () => {
         <Container fluid>
             <div className="d-flex justify-content-center fw-bold">Filter</div>
             <Row >
-                <div className="store-filter">
-                <button className="button market-button" id="button-menu">
-                    Type
-                    <Image src={dropdownLogo} className="drop-logo"/>
-                </button>
-                </div>
-                <div className="store-filter">
-                    <button className="button market-button" id="button-menu">Price
-                        <Image src={dropdownLogo} className="drop-logo"/>
-                    </button>
-                </div>
-                    <div className="store-filter">
-                        <button className="button market-button" id="button-menu">Location
-                            <Image src={dropdownLogo} className="drop-logo"/>
-                        </button>
-                    </div>
-                    <div className="store-filter">
-                        <button className="button market-button mb-1" id="button-menu">Rating
-                            <Image src={dropdownLogo} id="flip-dropdown" className="drop-logo"
-                            />
-                        </button>
+                <Accordion className="menu-accordion">
+                    <Accordion.Item eventKey="0" className="mb-5 mt-5 flush">
+                        <Accordion.Header>Type</Accordion.Header>
+                        <Accordion.Body>
 
-                        <div className="w-100 m-0">
-                            <Image src={star} alt="rating" className="rating-stars"/>
-                            <Image src={star} alt="rating" className="rating-stars"/>
-                            <Image src={star} alt="rating" className="rating-stars"/>
-                            <Image src={star} alt="rating" className="rating-stars"/>
-                            <Image src={star} alt="rating" className="rating-stars"/>
-                        </div>
-                        <div className="w-100 m-0">
-                            <Image src={star} alt="rating" className="rating-stars"/>
-                            <Image src={star} alt="rating" className="rating-stars"/>
-                            <Image src={star} alt="rating" className="rating-stars"/>
-                            <Image src={star} alt="rating" className="rating-stars"/>
-                            <Image src={unfilledStar} alt="rating" className="rating-stars"/>
-                        </div>
-                        <div className="w-100 m-0">
-                            <Image src={star} alt="rating" className="rating-stars"/>
-                            <Image src={star} alt="rating" className="rating-stars"/>
-                            <Image src={star} alt="rating" className="rating-stars"/>
-                            <Image src={unfilledStar} alt="rating" className="rating-stars"/>
-                            <Image src={unfilledStar} alt="rating" className="rating-stars"/>
-                        </div>
-                        <div className="w-100 m-0">
-                            <Image src={star} alt="rating" className="rating-stars"/>
-                            <Image src={star} alt="rating" className="rating-stars"/>
-                            <Image src={unfilledStar} alt="rating" className="rating-stars"/>
-                            <Image src={unfilledStar} alt="rating" className="rating-stars"/>
-                            <Image src={unfilledStar} alt="rating" className="rating-stars"/>
-                        </div>
-                        <div className="w-100 m-0">
-                            <Image src={star} alt="rating" className="rating-stars"/>
-                            <Image src={unfilledStar} alt="rating" className="rating-stars"/>
-                            <Image src={unfilledStar} alt="rating" className="rating-stars"/>
-                            <Image src={unfilledStar} alt="rating" className="rating-stars"/>
-                            <Image src={unfilledStar} alt="rating" className="rating-stars"/>
-                        </div>
-                    </div>
+                        </Accordion.Body>
+                    </Accordion.Item>
+                    <Accordion.Item eventKey="1" className="mb-5 flush">
+                        <Accordion.Header>Price</Accordion.Header>
+                        <Accordion.Body>
+                           <div>
 
+                           </div>
+
+                        </Accordion.Body>
+                    </Accordion.Item>
+                    <Accordion.Item eventKey="2" className="mb-5 flush">
+                        <Accordion.Header>Location</Accordion.Header>
+                        <Accordion.Body>
+
+                        </Accordion.Body>
+                    </Accordion.Item>
+                    <Accordion.Item eventKey="3" className="mb-5 flush">
+                        <Accordion.Header>Rating</Accordion.Header>
+                        <Accordion.Body>
+
+
+                        </Accordion.Body>
+                    </Accordion.Item>
+                </Accordion>
             </Row>
 
         </Container>
