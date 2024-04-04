88600579a59cc0c72a8a02e9785d67e055f90939
Ryan <ryanloi273@gmail.com>
2024-04-04 04:56:46 -0600

    feat(hub): Add functionality to retrieve all hubs
    Modified hub.controller.js to implement a new function for fetching all hubs stored in the database. Additionally, updated hub.routes.js to route the new function appropriately.
    
    feat(PostToggle.jsx): Update to fetch all hub titles
    
    Updated PostToggle.jsx to fetch all hub titles from the database in order to populate the dropdown menu with available hub options.
    
    BREAKING CHANGE: None

--- api/controllers/hub.controller.js
+++ api/controllers/hub.controller.js
@@ -1,4 +1,12 @@
-// temp function
-export const tempfunction = (req, res)=>{
-    res.send("Temp Function!");
+import Hub from '../models/hub.model.js';
+
+export const getAllHubs = async (req, res) => {
+    try {
+        // Fetch all hubs from the database
+        const hubs = await Hub.find();
+        res.status(200).json(hubs);
+    } catch (error) {
+        res.status(500).send("Error fetching hubs");
+        console.error(error);
+    }
 };
