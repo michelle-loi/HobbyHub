f2a061be291fa32b8ae85b939961b306e3104997
Ryan <ryanloi273@gmail.com>
2024-04-06 17:23:26 -0600

    feat(hub.model.js): Modify fields for improved hub management
    Updated hub.model.js to modify certain fields, enhancing the management capabilities of hubs within the platform. This modification ensures better organization and usability of hub-related data.
    
    BREAKING CHANGE: None

--- api/models/hub.model.js
+++ api/models/hub.model.js
@@ -2,7 +2,7 @@ import mongoose from 'mongoose';
 const { Schema } = mongoose;
 
 const hubSchema = new Schema({
-    title: {
+    hubName: {
         type: String,
         required: true,
         unique: true,
