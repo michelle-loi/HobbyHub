c2f24407333c11f970eb60bffebd4fbdd0baec11
janpetallo <101074771+janpetallo@users.noreply.github.com>
2024-04-06 23:54:46 -0600

    feat(search-bar-desktop): ensures fetching posts depends on category selected

--- client/src/pages/search-results/SearhResults.jsx
+++ client/src/pages/search-results/SearhResults.jsx
@@ -4,16 +4,24 @@ import {Button, Container, Nav, Navbar, NavDropdown, Offcanvas} from "react-boot
 import Posts from "../../components/desktop/posts/Posts.jsx";
 import Post from "../../components/desktop/post/Post.jsx";
 import newRequest from "../../utilities/newRequest.js";
+import {useLocation} from "react-router-dom";
+
 
 const SearchReults = ({hubTitle, postAll= true})  => {
+     // get URL parameters
+     const location = useLocation();
+     const searchParams = new URLSearchParams(location.search);
+     const search = searchParams.get("query");
+     const category = searchParams.get("category");
+
     const [posts, setPosts] = useState([]);
 
-        useEffect(() => {
+    useEffect(() => {
 
         // Function to fetch all posts from backend API server
         const fetchPosts = async () => {
             try {
-                if(postAll === true) {
+                if(postAll === true && category === 'Hubs') {
                     const response = await newRequest.get("/posts/getAllPosts");
                     if (response.status !== 200) {
                         throw new Error("Failed to fetch posts");
@@ -21,31 +29,32 @@ const SearchReults = ({hubTitle, postAll= true})  => {
                     const postsData = response.data;
                     setPosts(postsData);
 
-                }else{
-                    // get currentUser
-                    const currentUser = JSON.parse(localStorage.getItem("currentUser"));
-
-                    // get the user data
-                    const response = await newRequest.get(`/users/getUser/${currentUser._id}`);
-                    if (response.status === 200) {
-                        // get the user's post id's
-                        const userPosts = response.data.posts;
-
-
-
-                        // now we try to get the posts associated with all of the post id's
-                        try {
-                            const response2 = await newRequest.get(`/posts/getPostsByIds?postIDs=${userPosts.join(',')}`);
-                            setPosts(response2.data);
-
-                        }catch (error){
-                            console.log("Error getting all posts");
-                        }
-
-                    } else {
-                        console.log("Error getting user data from server");
-                    }
+                } else if(postAll === true && category === 'Market') {
+                    // Do nothing for now
                 }
+                // else{
+                //     // get currentUser
+                //     const currentUser = JSON.parse(localStorage.getItem("currentUser"));
+
+                //     // get the user data
+                //     const response = await newRequest.get(`/users/getUser/${currentUser._id}`);
+                //     if (response.status === 200) {
+                //         // get the user's post id's
+                //         const userPosts = response.data.posts;
+
+                //         // now we try to get the posts associated with all of the post id's
+                //         try {
+                //             const response2 = await newRequest.get(`/posts/getPostsByIds?postIDs=${userPosts.join(',')}`);
+                //             setPosts(response2.data);
+
+                //         }catch (error){
+                //             console.log("Error getting all posts");
+                //         }
+
+                //     } else {
+                //         console.log("Error getting user data from server");
+                //     }
+                // }
             } catch (error) {
                 console.error(error);
             }
@@ -55,6 +64,7 @@ const SearchReults = ({hubTitle, postAll= true})  => {
 
     }, []); // Empty dependency array ensures this effect runs only once when component mounts
 
+   
 
 
 
@@ -67,7 +77,6 @@ const SearchReults = ({hubTitle, postAll= true})  => {
 
     return (
         
-
 
         <div className = "posts">
             {posts.map(post=>(
