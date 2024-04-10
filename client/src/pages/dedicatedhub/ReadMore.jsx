368b1599a9277c051cb6070bca05374016207014
Ryan <ryanloi273@gmail.com>
2024-04-10 04:52:25 -0600

    feat(ReadMore.jsx): Enhance component to display dynamic content
    Updated ReadMore.jsx to accept props that enable it to display the correct description, rules, and resources based on the provided data. This modification improves the flexibility and reusability of the component, allowing it to adapt to various content types and scenarios.
    
    BREAKING CHANGE: None

--- client/src/pages/dedicatedhub/ReadMore.jsx
+++ client/src/pages/dedicatedhub/ReadMore.jsx
@@ -2,98 +2,31 @@ import React from "react";
 import {Col, Row} from "react-bootstrap";
 import "./ReadMore.scss"
 
-const ReadMore = () => {
-
-    const description = "Welcome to the dedicated hub for speedrunners! Share your runs," +
-        " compete with others and just have an overall great time."
+const ReadMore = ({Description, Rules, Resources}) => {
 
     return (
         <>
             <Row className="read-more-section">
                 <Col>
                     <h5>Description</h5>
-                    <p>{description}</p>
+                    <div className="RM-hubDescription" dangerouslySetInnerHTML={{ __html: Description }} />
                 </Col>
             </Row>
 
             <Row className="read-more-section">
                 <Col>
                     <h5>Rules</h5>
-                    <ol>
-                        <li>
-                            Respect others
-                        </li>
-                        <li>
-                            Be Civil
-                        </li>
-                        <li>
-                            No toxic posting
-                        </li>
-                        <li>
-                            Have fun
-                        </li>
-                    </ol>
+                    <div className="RM-HubRules" dangerouslySetInnerHTML={{ __html: Rules }} />
                 </Col>
             </Row>
 
             <Row className="read-more-section">
                 <Col>
                     <h5>Resources</h5>
-                    <div>
-                        Want to get into speedrunning here are some great links to start:
-                        <ul>
-                            <li>
-                                "https://www.speedrun.com/"
-                            </li>
-
-                            <li>
-                                "https://livesplit.org/"
-                            </li>
-
-                            <li>
-                                "https://obsproject.com/"
-
-                            </li>
-
-                            <li>
-                                "https://www.xsplit.com/"
-                            </li>
-                        </ul>
-                    </div>
+                    <div className="RM-hubResource" dangerouslySetInnerHTML={{ __html: Resources }} />
                 </Col>
             </Row>
 
-            <Row className="read-more-section">
-                <Col>
-                    <h5>Test too much content can scroll</h5>
-                    <p>
-                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium autem dignissimos dolore
-                        impedit modi perferendis possimus quia ut voluptates? Animi corporis delectus dolorem eius et
-                        illo ipsam minima obcaecati voluptatibus.
-                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium autem dignissimos dolore
-                        impedit modi perferendis possimus quia ut voluptates? Animi corporis delectus dolorem eius et
-                        illo ipsam minima obcaecati voluptatibus.
-                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium autem dignissimos dolore
-                        impedit modi perferendis possimus quia ut voluptates? Animi corporis delectus dolorem eius et
-                        illo ipsam minima obcaecati voluptatibus.
-                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium autem dignissimos dolore
-                        impedit modi perferendis possimus quia ut voluptates? Animi corporis delectus dolorem eius et
-                        illo ipsam minima obcaecati voluptatibus.
-                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium autem dignissimos dolore
-                        impedit modi perferendis possimus quia ut voluptates? Animi corporis delectus dolorem eius et
-                        illo ipsam minima obcaecati voluptatibus.
-                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium autem dignissimos dolore
-                        impedit modi perferendis possimus quia ut voluptates? Animi corporis delectus dolorem eius et
-                        illo ipsam minima obcaecati voluptatibus.
-                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium autem dignissimos dolore
-                        impedit modi perferendis possimus quia ut voluptates? Animi corporis delectus dolorem eius et
-                        illo ipsam minima obcaecati voluptatibus.
-                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium autem dignissimos dolore
-                        impedit modi perferendis possimus quia ut voluptates? Animi corporis delectus dolorem eius et
-                        illo ipsam minima obcaecati voluptatibus.
-                    </p>
-                </Col>
-            </Row>
         </>
     )
 }
