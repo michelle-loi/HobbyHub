013d8943667e4e81112c2a2b85002a7e0582e1c4
Ryan <ryanloi273@gmail.com>
2024-04-10 04:33:31 -0600

    feat(DedicatedHub.jsx): Retrieve hub data using hub name from state
    Updated DedicatedHub.jsx to fetch hub data using the hub name obtained from state passing. This modification enhances the component's functionality by ensuring accurate retrieval of hub information based on the provided hub name, improving the user experience within the dedicated hub view.
    
    BREAKING CHANGE: None

--- client/src/pages/dedicatedhub/DedicatedHub.jsx
+++ client/src/pages/dedicatedhub/DedicatedHub.jsx
@@ -1,9 +1,46 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import "./DedicatedHub.scss"
 import Posts from "../../components/desktop/posts/Posts.jsx";
 import DedicatedHubHeader from "./DedicatedHubHeader.jsx";
+import {useLocation, useNavigate} from "react-router-dom";
+import newRequest from "../../utilities/newRequest.js";
 
 const DedicatedHub = () => {
+
+    // get the hub name passed to dedicated hub when clicking on the hub
+    const location = useLocation();
+    const hub = location.state?.hub;
+
+    const [hubData, setHubData] = useState();
+
+    // navigation hook
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        // make sure hub data is present otherwise the user is trying to load /hubs manually instead of through the menu
+        // so navigate back to the home page to prevent errors
+        const fetchHubData = async () => {
+            try {
+                if (!hub) {
+                    navigate("/");
+                } else {
+                    const response = await newRequest.get(`/hubs/getHub/${hub}`);
+                    if (response.status === 200) {
+                        setHubData(response.data);
+                    } else {
+                        throw new Error(`Failed to fetch hub data. Status: ${response.status}`);
+                    }
+                }
+            } catch (error) {
+                // upon error log the error and navigate back home
+                console.log(error);
+                navigate("/");
+            }
+        };
+
+        fetchHubData();
+    }, [hub, navigate]);
+
 
     return (
         <>
