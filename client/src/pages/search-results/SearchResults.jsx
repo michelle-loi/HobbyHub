1002e44fa7ebafa366565b11872c2f096f95a695
janpetallo <101074771+janpetallo@users.noreply.github.com>
2024-04-12 10:31:19 -0600

    fix(search-results): resolved TypeError when going from Market Search to Hubs Search
    Separated hubsPosts and marketPosts useStates to fix this

--- client/src/pages/search-results/SearchResults.jsx
+++ client/src/pages/search-results/SearchResults.jsx
@@ -16,7 +16,8 @@ const SearchReults = ({hubTitle = true, postAll= true})  => {
      const category = searchParams.get("category");
 
     // get all posts
-    const [posts, setPosts] = useState([]);
+    const [hubsPosts, setHubPosts] = useState([]);
+    const [marketPosts, setMarketPosts] = useState([]);
 
     useEffect(() => {
 
@@ -28,17 +29,18 @@ const SearchReults = ({hubTitle = true, postAll= true})  => {
                     if (response.status !== 200) {
                         throw new Error("Failed to fetch posts");
                     }
-                    const postsData = response.data;
-                    setPosts(postsData);
+                    const hubPostsData = response.data;
+                    setHubPosts(hubPostsData);
+                    console.log(hubPostsData);
 
                 } else if(postAll === true && category === 'Market') {
                     const response = await newRequest.get("/marketPlacePosts/getAllMarketPlacePosts");
                     if (response.status !== 200) {
                         throw new Error("Failed to fetch posts");
                     }
-                    const postsData = response.data;
-                    setPosts(postsData);
-                    console.log(postsData);
+                    const marketPostsData = response.data;
+                    setMarketPosts(marketPostsData);
+                    console.log(marketPostsData);
                 } 
             } catch (error) {
                 console.error(error);
@@ -50,20 +52,23 @@ const SearchReults = ({hubTitle = true, postAll= true})  => {
     }, [postAll, category]); 
     
     // filter posts based on search query
-    const [filteredPosts, setFilteredPosts] = useState([]);
+    const [filteredHubPosts, setFilteredHubPosts] = useState([]);
+    const [filteredMarketPosts, setFilteredMarketPosts] = useState([]);
 
     useEffect(() => {
         const filterPosts = () => {
             if (searchQuery !== '' && category === 'Hubs') {
-                const newFilteredPosts = posts.filter(post =>
+                console.log("Filtering Hubs")
+                const newFilteredPosts = hubsPosts.filter(post =>
                     post.title?.toLowerCase().trim().includes(searchQuery) ||
                     post.description?.toLowerCase().trim().includes(searchQuery) ||
                     post.userName?.toLowerCase().trim().includes(searchQuery) ||
                     post.hubName?.toLowerCase().trim().includes(searchQuery)
                 );
-                setFilteredPosts(newFilteredPosts);
+                setFilteredHubPosts(newFilteredPosts);
             } else if (searchQuery !== '' && category === 'Market') {
-                const newFilteredPosts = posts.filter(post =>
+                console.log("Filtering Market")
+                const newFilteredPosts = marketPosts.filter(post =>
                     post.title?.toLowerCase().trim().includes(searchQuery) ||
                     post.description?.toLowerCase().trim().includes(searchQuery) ||
                     post.username?.toLowerCase().trim().includes(searchQuery) ||
@@ -72,36 +77,42 @@ const SearchReults = ({hubTitle = true, postAll= true})  => {
                     post.condition?.toLowerCase().trim().includes(searchQuery) ||
                     post.email?.toLowerCase().trim().includes(searchQuery)
                 );
-                setFilteredPosts(newFilteredPosts);
-            } else {
-                setFilteredPosts(posts);
+                setFilteredMarketPosts(newFilteredPosts);
+            } else if (searchQuery === '' && category === 'Hubs') {
+                setFilteredHubPosts(hubsPosts);
+            } else if (searchQuery === '' && category === 'Market') {
+                setFilteredMarketPosts(marketPosts);
             }
         };
         filterPosts();
-    }, [searchQuery, posts, category]);
+    }, [searchQuery, category, hubsPosts, marketPosts]);
 
     return (
         <>
             <div className="search-posts">
                 <div className="p-1">
-                    {filteredPosts.length > 0 && <h3 className="page-title">{searchQuery === "" ? "Suggested Posts: ":'Posts related to "' + searchParams.get('query').trim() + '"'}</h3>}
-                    {filteredPosts.length === 0 && searchQuery === "" && <h3>No posts to show</h3>}
+                    {category === 'Hubs' && (filteredHubPosts.length > 0) && <h3 className="page-title">{searchQuery === "" ? "Suggested Posts: ":'Posts related to "' + searchParams.get('query').trim() + '"'}</h3>}
+                    {category === 'Market' && (filteredMarketPosts.length > 0) && <h3 className="page-title">{searchQuery === "" ? "Suggested Items: ":'Items related to "' + searchParams.get('query').trim() + '"'}</h3>}
+                    {category === 'Hubs' && (filteredHubPosts.length === 0) && searchQuery === "" && <h3>No posts to show</h3>}
+                    {category === 'Market' && (filteredMarketPosts.length === 0) && searchQuery === "" && <h3>No items to show</h3>}
                 </div>
                 <div className="content">
                     <div className="card-columns">
-                        {filteredPosts.length === 0 && searchQuery !== "" && <h3>No post related to "{searchParams.get('query').trim()}"</h3>}
-                        <p className="search-count p-1">Total search results: {filteredPosts.length}</p>
-                        
-                        {/* {filteredPosts.slice().reverse().map(post => (
-                            <Post hubTitle={hubTitle} post={post} isPopup={false} key={post._id} />
-                        ))} */}
-
+                        {category === 'Hubs' &&  filteredHubPosts.length === 0 && searchQuery !== "" && <h3>No post related to "{searchParams.get('query').trim()}"</h3>}
+                        {category === 'Market' && filteredMarketPosts.length === 0 && searchQuery !== "" && <h3>No item related to "{searchParams.get('query').trim()}"</h3>}
+                        {category === 'Hubs' && <p className="search-count p-1">Total search results: {filteredHubPosts.length}</p> }
+                        {category === 'Market' && <p className="search-count p-1">Total search results: {filteredMarketPosts.length}</p> } 
+{
+                        console.log(category)
+}
                         {category === 'Hubs' ? 
-                            filteredPosts.slice().reverse().map(post => (
+                            console.log("Hubs mapping") ||
+                            filteredHubPosts.slice().reverse().map(post => (
                                 <Post hubTitle={hubTitle} post={post} isPopup={false} key={post._id} />
                             )) :
+                            console.log("Market mapping") ||
                             <div className="item-card-wrapper">
-                                {filteredPosts.slice().reverse().map(post => (
+                                {filteredMarketPosts.slice().reverse().map(post => (
                                     <ItemCard item={post} key={post._id} />
                                 ))}
                             </div>
